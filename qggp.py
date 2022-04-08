bc6e36b345cad714f5f0f131a5d83b541c4016ad
rifqimf12 <rifqi12muhf@gmail.com>
2022-04-08 16:04:32 +0700

    feat add goproxy var

--- qggp.py
+++ qggp.py
@@ -5,6 +5,8 @@ from config import GOPRIVATE, PRIVATE_LIBRARIES
 
 args = sys.argv
 if len(args) > 1:
-    os.system(f"GOPRIVATE={GOPRIVATE} go get {PRIVATE_LIBRARIES[args[1]]}")
+    os.system(
+        f"GOPROXY=https://goproxy.io,direct GOPRIVATE={GOPRIVATE} go get {PRIVATE_LIBRARIES[args[1]]}"
+    )
 else:
     print("pass module-name's key (that been registered on config.py) as arg")
